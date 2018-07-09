518e511b9ea6bd4acd698ae24ff9db6f3a5749c4
greatelv <greatelv@gmail.com>
2018-07-09 17:42:26 +0900

    vue.js를 이용한 템플레이팅 및 스타일 개선

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,6 @@
+
 const profile = {
+  profilePhoto: 'assets/images/profile.jpg',
   name: '전태경 Jay',
   job: '소프트웨어 엔지니어',
   mail: 'greatelv@gmail.com',
@@ -11,16 +13,70 @@ const profile = {
 const educations = [
   {
     degree: '컴퓨터공학 학사',
-    school: '서울 과학 기술대학교',
-    time: '2011 - 2012'
+    school: '서울과학기술대학교',
+    time: '2008 - 2015'
+  },
+  {
+    degree: '졸업',
+    school: '서울경신고등학교',
+    time: '2005 - 2008'
   }
 ]
+
+const careerSummary = [
+  '현재는 일을 좀 쉬고 세계를 여행중입니다.</br>',
+  'I want to make common sense world.',
+].join('');
+
+const careers = [
+  {
+    jobTitle: 'Senior Software Engineer',
+    time: '2015.3 - 2018.7',
+    company: 'EA Korea, 서울',
+    details: '레이싱 게임 개발',
+  },
+  {
+    jobTitle: 'Technical Lead',
+    time: '2014.7 - 2015.2',
+    company: '핸디썸, 대구',
+    details: '핸디라이트 개발',
+  },
+  {
+    jobTitle: '선임 연구원',
+    time: '2012.6 - 2014.4',
+    company: 'Handstudio, 서울',
+    details: '스마트TV 개발',
+  },
+];
+
+const projects = [
+  {
+    title: 'Need For Speed EDGE',
+    link: '#',
+    detail: 'hahaha',
+  },
+  {
+    title: 'Need For Speed EDGE',
+    link: '',
+    detail: 'hahaha',
+  }
+];
+
+const skills = [
+  'Typescript', 'Javascript', 'Angular', 'Nodejs', 'ES6', 'Restful', 'HTML', 'CSS',
+  'Webpack', 'Gulp', 'Browserify', 'React', 'Redux', 'Lodash', 'jQuery', 'Vuejs',
+  'PhantomJS', 'Jasmine',
+  'Git', 'Perforce', 'Subversion',
+  'Java', 'Spring', 'Mybatis', 'Jersey', 'SQL', 'JSP/Servlet',
+  'Jenkins', 'JIRA', 'Confluence',
+  'Atom IDE'
+];
 
 class Resume extends Vue {
   constructor () {
     super({
       el: '#resume',
-      data: { profile, educations },
+      data: { profile, educations, careerSummary, careers, skills },
       created: () => {},
       mounted: () => {}  //after rendering
     })
